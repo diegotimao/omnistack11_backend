fc485d1c55c9445cb072dc361fbd92d17e9bf116
diegotimao <diegotimao1104@gmail.com>
2020-03-24 20:11:38 -0300

    Aula 01

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const express = require('express');
 
 const app = express( );
 
+app.get('/', (request, response) => {
+  return response.json( {
+    evento: 'Semana Oministack 11.0',
+    aluno: 'Diego Cavalcanti'
+
+  } );
+});
+
 app.listen(3333);
+
+
